526f8c8d92d5aba91ecc1a4ee5ca2e23c7bfb0a6
azix-khan <azixkhan.55@gmail.com>
2024-08-14 13:42:10 +0500

    Leave a Review UI

---
+++ android/app/src/main/kotlin/com/example/leave_a_review/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.leave_a_review
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:leave_a_review/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+        textTheme: TextTheme(
+          displayLarge:
+              GoogleFonts.poppins(fontSize: 50, fontWeight: FontWeight.w900),
+          displayMedium:
+              GoogleFonts.poppins(fontSize: 22, fontWeight: FontWeight.w600),
+          displaySmall:
+              GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500),
+          bodyMedium:
+              GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.normal),
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
